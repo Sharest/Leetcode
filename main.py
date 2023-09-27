df4927f9efce9302a033357c47a1a7767074df12
Sharest <efremov.dima.2016@gmail.com>
2023-09-27 12:30:52 +0300

    Move Zeroes

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
-    return_nums = []
-    if len(nums1) >= len(nums2):
-        for num in nums2:
-            if num in nums1:
-                return_nums.append(num)
-                nums1.remove(num)
-    else:
-        for num in nums1:
-            if num in nums2:
-                return_nums.append(num)
-                nums2.remove(num)
+def moveZeroes(nums: list[int]) -> None:
+    zero_arr = []
+    for _ in range(nums.count(0)):
+        nums.remove(0)
+        zero_arr.append(0)
 
-    return return_nums
+    nums.extend(zero_arr)
