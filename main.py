7b1e45f70630d7bf2b64903f0e9b990d2bb84c42
Sharest <efremov.dima.2016@gmail.com>
2023-09-27 11:57:30 +0300

    Contains Duplicate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def maxProfit(prices: list[int]) -> int:
-    min_num = prices[0]
-    profit = 0
-
-    for i in range(1, len(prices)):
-        profit = max(profit, prices[i]-min_num)
-        min_num = min(min_num, prices[i])
+def containsDuplicate(nums: list[int]):
+    if len(set(nums)) == len(nums):
+        return False
+    else:
+        return True
