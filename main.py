896a539f3f2b4edfdf9e8a6372d9898e1613ebce
Sharest <efremov.dima.2016@gmail.com>
2023-09-27 12:13:21 +0300

    Intersection of Two Arrays II

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def containsDuplicate(nums: list[int]):
-    if len(set(nums)) == len(nums):
-        return False
+def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
+    return_nums = []
+    if len(nums1) >= len(nums2):
+        for num in nums2:
+            if num in nums1:
+                return_nums.append(num)
+                nums1.remove(num)
     else:
-        return True
+        for num in nums1:
+            if num in nums2:
+                return_nums.append(num)
+                nums2.remove(num)
+
+    return return_nums
