b2eb4186e3c4228ec9c55eb18d7d48bba3355bff
Sharest <efremov.dima.2016@gmail.com>
2023-10-07 14:00:11 +0300

    Plus One

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def removeDuplicates(nums: list[int]) -> int:
-    k = 1
-    for i in range(1, len(nums)):
-        if nums[i] != nums[i-1]:
-            nums[k] = nums[i]
-            k += 1
-    return k
+def plusOne(digits: list[int]) -> list[int]:
+    num = 0
+
+    for i in range(1, len(digits)+1):
+        num = num + digits[-i] * (10**(i-1))
+
+    num = num + 1
+    return_nums = []
+    while num != 0:
+        z = num % 10
+        return_nums.append(z)
+        num = num // 10
+
+    return_nums.reverse()
+    return return_nums
