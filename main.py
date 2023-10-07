36663f41411b2f05b60144b8b8af1509a67bafb9
Sharest <efremov.dima.2016@gmail.com>
2023-10-07 15:39:54 +0300

    Merge Sorted Array

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-def plusOne(digits: list[int]) -> list[int]:
-    num = 0
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    for i in range(n):
+        nums1[m+i] = nums2[i]
+    nums1.sort()
+    
 
-    for i in range(1, len(digits)+1):
-        num = num + digits[-i] * (10**(i-1))
-
-    num = num + 1
-    return_nums = []
-    while num != 0:
-        z = num % 10
-        return_nums.append(z)
-        num = num // 10
-
-    return_nums.reverse()
-    return return_nums
+print(merge(nums1 = [-1,0,0,3,3,3,0,0,0], m = 6, nums2 = [1,2,2], n = 3))
