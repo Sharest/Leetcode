cbef48785029e6357b47ec13f86694d2aad3ac18
Sharest <efremov.dima.2016@gmail.com>
2023-10-09 21:21:23 +0300

    First Unique Character in a String

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def fizzBuzz(n: int) -> list[str]:
-    result = []
-    for i in range(1, n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        else:
-            result.append(str(i))
-    return result
-
-
-print(fizzBuzz(15))
+def firstUniqChar(s: str) -> int:
+    for i in s:
+        if s.count(i) == 1:
+            return s.index(i)
+    return -1
