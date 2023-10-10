87409c05af289c2d8ee02fe5a10c392884e290cf
Sharest <efremov.dima.2016@gmail.com>
2023-10-10 17:08:22 +0300

    Valid Anagram

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        if n < 1:
-            return False
-        elif n == 1:
-            return True
-            
-        while n % 3 == 0:
-            if n == 3:
-                return True
-            else:
-                n //= 3
-        
-        if n % 3 != 0:
-            return False
+def isAnagram(s: str, t: str) -> bool:
+    s_ls = []
+    t_ls = []
 
+    s_ls.extend(s)
+    t_ls.extend(t)
+    s_ls.sort()
+    t_ls.sort()
+    if s_ls == t_ls:
+        return True
+    else:
+        return False
+
+print(isAnagram(s = "rat", t = "car"))
